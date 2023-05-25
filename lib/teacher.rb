d03933a2adf0a03d255074f86d65962814da5f7c
Juma <juma.musa@student.moringaschool.com>
2023-05-25 17:14:29 +0300

    done

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,22 @@
-class Teacher
+class User
+  attr_accessor :first_name, :last_name
+end
 
-  KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
+class Teacher < User
+  def initialize
+    @knowledge = [
+      "a String is a type of data in Ruby",
+      "programming is hard, but it's worth it",
+      "javascript async web request",
+      "Ruby method call definition",
+      "object oriented dog cat class instance",
+      "class method class variable instance method instance variable",
+      "programming computers hacking learning terminal",
+      "bash Ruby rvm update certs"
+    ]
+  end
 
-  
+  def teach
+    @knowledge.sample
+  end
 end
